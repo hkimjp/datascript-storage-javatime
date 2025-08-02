f73af32ff2ab57a1e467e3c75549a2a897bb32b5
hkimjp <hkim@hkim.jp>
2025-08-02 13:39:41 +0900

    fix gitignore

--- dev/user.clj
+++ dev/user.clj
@@ -2,7 +2,8 @@
   (:require
    [clj-reload.core :as reload]
    [datascript.core :as d]
-   [hkimjp.datascript :refer [schema storage create-conn restore-conn close-conn]]
+   [hkimjp.datascript
+    :refer [schema storage create-conn restore-conn close-conn]]
    [java-time.api :as jt]))
 
 (reload/init
@@ -19,29 +20,29 @@
   (d/transact! conn [{:db/id -1, :time (jt/local-date-time)}])
 
   (close-conn)
+
+  (def conn (restore-conn storage))
+
+  (d/transact! conn [{:db/id -1, :name "bonbay sapphire"}])
+  (d/transact! conn [{:db/id -1, :date (jt/local-date)}])
+  (d/transact! conn [{:db/id -1, :name "deacon" :date (jt/local-date)}])
+
+  (d/q '[:find (max ?e)
+         :where
+         [?e _ _]]
+       @conn)
+
+  (d/q '[:find ?e ?time ?name
+         :keys id time name
+         :in $ ?name
+         :where
+         [?e :date ?time]]
+       @conn
+       ["daecon" "bonbay sapphire"])
+
+  (d/pull @conn '[*] 4)
+
+  (jt/plus (:time (d/pull @conn '[*] 2) (jt/days 1)))
+
+  (close-conn)
   :rcf)
-
-(def conn (restore-conn storage))
-
-(d/transact! conn [{:db/id -1, :name "bonbay
-(d/transact! conn [{:db/id -1, :date (jt/local-date)}])
-(d/transact! conn [{:db/id -1, :name "deacon" :date (jt/local-date)}])
-
-(d/q '[:find (max ?e)
-       :where
-       [?e _ _]]
-     @conn)
-
-(d/q '[:find ?e ?time ?name
-       :keys id time name
-       :in $ ?name
-       :where
-       [?e :date ?time]]
-     @conn
-     ["daecon" "bonbay"])
-
-(d/pull @conn '[*] 4)
-
-(jt/plus (:time (d/pull @conn '[*] 2) (jt/days 1)))
-
-(close-conn)
