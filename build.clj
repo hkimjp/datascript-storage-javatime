5e1112b9a388041dfb3650c20fe36c07bf91da21
hkimjp <hkim@hkim.jp>
2025-10-04 10:08:08 +0900

    datascript updated to 0.7.5

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'io.github.hkimjp/datascript-storage-jatavime)
-(def version "0.7.4")
+(def version "0.7.5")
 (def main 'hkimjp.datascript)
 (def class-dir "target/classes")
 
